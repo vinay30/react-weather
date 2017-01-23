3e9047ac8b1e7a0257aec42074caa923dbeefba8
Vinay <vinay.punwani@shopify.com>
2017-01-23 05:36:30 -0800

    [ReactWeather] Update examples page with new styles

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,22 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = props => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>This is the examples page.</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few examples to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Vancouver'>Vancouver, BC</Link>
+        </li>
+        <li>
+          <Link to='/?location=Montreal'>Montreal, QC</Link>
+        </li>
+        <li>
+          <Link to='/?location=Ottawa'>Ottawa, ON</Link>
+        </li>
+      </ol>
     </div>
   );
 };
